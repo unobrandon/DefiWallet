da40429d1513f450d372f00aab89ccac5f4eb727
Brandon Shaw <unobrandon@mac.com>
2022-02-12 15:15:53 -0500

    Added constants

---
+++ Shared/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  DefiWallet
+//
+//  Created by Brandon Shaw on 2/12/22.
+//
+
+import SwiftUI
+
+final class Constants {
+    
+    static let deviceType = UIDevice.current.userInterfaceIdiom
+
+}
