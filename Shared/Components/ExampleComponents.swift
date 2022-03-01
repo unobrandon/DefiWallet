b7c51e6416904a91fa02161d19b5206b8c794ebb
Brandon Shaw <unobrandon@mac.com>
2022-02-28 20:01:49 -0500

    Added moving numbers

---
+++ Shared/Components/ExampleComponents.swift
@@ -0,0 +1,20 @@
+//
+//  ExampleComponents.swift
+//  DefiWallet
+//
+//  Created by Brandon Shaw on 2/28/22.
+//
+
+import SwiftUI
+
+struct ExampleComponents: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExampleComponents_Previews: PreviewProvider {
+    static var previews: some View {
+        ExampleComponents()
+    }
+}
--- Shared/Views/Unauthenticated Views/Create Wallet Flow/CreateWalletView.swift
+++ Shared/Views/Unauthenticated Views/Create Wallet Flow/CreateWalletView.swift
@@ -14,6 +14,20 @@ struct CreateWalletView: View {
     @ObservedObject private var store: UserOnboardingServices
 
     @State var doneGenerating: Bool = false
+    @State var ethAddressLength: Double = 0
+
+    let timer = Timer.publish(every: 0.25, on: .main, in: .common).autoconnect()
+
+    var movingNumbersMask: some View {
+        return AnyView(LinearGradient(
+            gradient: Gradient(stops: [
+                Gradient.Stop(color: .clear, location: 0),
+                Gradient.Stop(color: .black, location: 0.2),
+                Gradient.Stop(color: .black, location: 0.8),
+                Gradient.Stop(color: .clear, location: 1.0)]),
+            startPoint: .top,
+            endPoint: .bottom))
+    }
 
     init(services: UnauthenticatedServices) {
         self.services = services
@@ -27,6 +41,26 @@ struct CreateWalletView: View {
             Color("baseBackground").ignoresSafeArea()
 
             VStack(alignment: .center, spacing: 20) {
+
+                HStack(alignment: .center, spacing: 0) {
+                    Text("0x")
+                        .fontTemplate(DefaultTemplate.caption)
+
+                    ForEach(0...3, id: \.self) { _ in
+                        MovingNumbersView(number: ethAddressLength, numberOfDecimalPlaces: 0, showComma: false) { str in
+                            Text(self.customLabelMapping(str))
+                                .fontTemplate(DefaultTemplate.caption)
+                        }
+                        .mask(movingNumbersMask)
+                    }
+                    .onReceive(timer) { _ in
+                        let low: Int = 1000000000
+                        let high: Int = 9999999999
+                        let randomInt = Int.random(in: low..<high)
+                        self.ethAddressLength = Double(randomInt)
+                    }
+                }
+
                 LoadingIndicator()
 
                 RoundedButton("Show Noti", style: .secondary, systemImage: "paperplane.fill", action: {
@@ -58,12 +92,15 @@ struct CreateWalletView: View {
                 }
 
                 ListSection(title: "Hello World", style: .border) {
-                    ListButtonStandard(title: "Come one man", systemImage: "safari", isLast: false, style: .border, action: {
+                    ListButtonStandard(title: "add up man", systemImage: "safari", isLast: false, style: .border, action: {
                         print("Come")
+                        ethAddressLength += 5.483
                     })
 
-                    ListButtonStandard(title: "Come one man", systemImage: "safari", isLast: true, style: .border, action: {
+                    ListButtonStandard(title: "sub down man", systemImage: "safari", isLast: true, style: .border, action: {
                         print("Come")
+                        ethAddressLength -= 8.624
+
                     })
                 }
 
@@ -76,7 +113,18 @@ struct CreateWalletView: View {
         .onAppear {
             DispatchQueue.main.asyncAfter(deadline: .now() + services.userOnboarding.generateWalletDelay) {
                 doneGenerating = true
+                self.timer.upstream.connect().cancel()
             }
         }
+        .onDisappear {
+            self.timer.upstream.connect().cancel()
+        }
     }
+
+    private func customLabelMapping(_ label: String) -> String {
+        guard let number = Int(label) else { return label }
+        let emojis = ["d", "t", "S", "3", "P", "k", "b", "2", "a", "z", "b", "5"]
+        return emojis[number]
+    }
+
 }
